f2c05c07798f923708c6317e5ab51adbcabdb808
Travis Mitchell <tmitch5151@gmail.com>
2021-11-29 12:25:56 -0800

    Updated Card Content

--- lib/utilities/globals.dart
+++ lib/utilities/globals.dart
@@ -5,44 +5,41 @@ import 'package:flutter/material.dart';
 String url = "https://heavenya.com/?form=donate";
 List<Map<String, dynamic>> quotes = [
   {
-    "front_title": "Page 1 FRONT Faith Practice",
-    "back_title": "Page 1 BACK TTILE Practice",
-    "back_description": "Page 1 BACK description",
-    "front_description":
-        "PAGE 1 Help out a stranger in need. If you see someone who is asking for help. Ask them what they need and see if you can be of service.",
+    "front_title": "Progressive Dinner",
+    "back_title": "Details To Consider",
+    "back_description": "Who do you want to go with? What application will you use to message them? What time are you going to meet? What are the addresses you are going to meet at? What address will people meet at initially? What should each people bring to be fully prepared? Should people have unique contribution responsibilities? "
+    "front_description": 
+        "A progressive dinner is where a dinner party with successive courses prepared and eaten at the residences of different hosts.",
     "color": [
       Colors.red.shade200,
       Colors.red.shade600,
     ],
   },
   {
-    "front_title": "Page 2 FRONT Faith Practice",
-    "back_title": "Page 2 BACK TTILE Practice",
-    "back_description": "Page 2 BACK description",
-    "front_description":
-        "Page 2 Help out a stranger in need. If you see someone who is asking for help. Ask them what they need and see if you can be of service.",
+    "front_title": "Listening Prayer",
+    "back_title": "Details To Consider",
+    "back_description": "Who do you want to go with? What application will you use to message them? What time are you going to meet? What is the address you are going to meet at? Do you want to have background music during this session? Approximately much time do you want to prayer and then share session to last for each person",
+        "front_description": "A listening prayer is where the group selects one person to pray for in the room for a given duration of time seeing if anything comes to mind to share with them.",
     "color": [
       Colors.blue.shade200,
       Colors.blue.shade600,
     ]
   },
   {
-    "front_title": "PAGE 3 FRONT Faith Practice",
-    "back_title": "PAGE 3 BACK TTILE Practice",
-    "back_description": "PAGE 3 BACK description",
-    "front_description":
-        "PAGE 3 Help out a stranger in need. If you see someone who is asking for help. Ask them what they need and see if you can be of service.",
+    "front_title": "Bible Project",
+    "back_title": "Details To Consider",
+    "back_description": "Is this a series that would have useful information for my small group? Is there a book in the bible that I would like to see animated? If so feel free to checkout https://www.youtube.com/c/bibleproject/featured.",
+        "front_description": "The bible project is a series of videos often accessed through YouTube that helps bring animated visualization to the Bible",
     "color": [
       Colors.green.shade200,
       Colors.green.shade600,
     ]
   },
   {
-    "front_title": "PAGE 4 FRONT Faith Practice",
-    "back_title": "PAGE 4 BACK TTILE Practice",
-    "back_description": "PAGE 4 BACK description",
-    "front_description":
-        "PAGE 4 Help out a stranger in need. If you see someone who is asking for help. Ask them what they need and see if you can be of service.",
+    "front_title": "Hike",
+    "back_title": "Details To Consider",
+    "back_description": "Who do you want to go with? What application will you use to message them? What time are you going to meet? What is the address you are going to meet at? What should people bring to be fully prepared? Side note there is an app called All Trails in the app store you can download to find local hiking trails near you https://www.alltrails.com/vv.",
+        "front_description": "Hiking is a great excercise activity to have quality conversations while exploring in nature.",
     "color": [
       Colors.purple.shade200,
       Colors.purple.shade600,
