7f3cdeedd7763071cbe8986460f967112ad931fa
Travis Mitchell <tmitch5151@gmail.com>
2021-12-15 08:55:14 -0800

    More Card Activities Added
    Improved App

--- lib/utilities/globals.dart
+++ lib/utilities/globals.dart
@@ -51,5 +51,221 @@ List<Map<String, dynamic>> quotes = [
       Colors.purple.shade200,
       Colors.purple.shade600,
     ]
-  }
+  },
+  {
+    "front_title": "Ice Skating",
+    "back_title": "Details To Consider",
+    "back_description":
+        "Who do you want to go with? What application will you use to message them? What time are you going to meet? What is the name of the ice skating rink? What is the address of the ice skating rink you are going to? Are you going to go together in one car or meet there separately? Do you own your own pair of skates or will you rent them?",
+    "front_description":
+        "Involves gliding across an ice surface on blades fixed to the bottoms of shoes known as skates. This can be a place to go especially during winter",
+    "color": [
+      Colors.blueAccent.shade200,
+      Colors.blueAccent.shade700,
+    ]
+  },
+  {
+    "front_title": "Roller Skating",
+    "back_title": "Details To Consider",
+    "back_description":
+        "Who do you want to go with? What application will you use to message them? What time are you going to meet? What is the name of the roller skating place? What is the address of the roller skating place you are going to? Are you going to go together in one car or meet there separately? Do you own your own pair of skates or will you rent them?",
+    "front_description":
+        "The activity or sport of gliding across a hard surface on roller skates. Most places will offer the option to use roller skates or roller blades.",
+    "color": [
+      Colors.indigo.shade100,
+      Colors.indigo.shade300,
+    ]
+  },
+  {
+    "front_title": "Movie Theater",
+    "back_title": "Details To Consider",
+    "back_description":
+        "Who do you want to go with? What application will you use to message them? What time are you going to meet? What is the name of the movie theater? What is the address of the movie theater you are going to? Are you going to go together in one car or meet there separately? Will you buy food there or have food at a different time?",
+    "front_description":
+        "Movie theaters often show new movies. Movies are a sequence of consecutive still images recorded in a series to be viewed on a screen in such rapid succession as to give the illusion of natural movement.",
+    "color": [
+      Colors.brown.shade200,
+      Colors.brown.shade700,
+    ]
+  },
+  {
+    "front_title": "Axe Throwing",
+    "back_title": "Details To Consider",
+    "back_description":
+        "Who do you want to go with? What application will you use to message them? What time are you going to meet? What is the name of the axe throwing place? What is the address of the axe throwing place you are going to? Are you going to go together in one car or meet there separately?",
+    "front_description":
+        "Similar to darts but you throw an axe at a target attempting to hit the bullseye as near as possible. Historically it has been an event in lumberjack competitions.",
+    "color": [
+      Colors.brown.shade100,
+      Colors.brown.shade900,
+    ]
+  },
+  {
+    "front_title": "White Elephant Gift Exchange",
+    "back_title": "Details To Consider",
+    "back_description":
+        "Who do you want to come? What application will you use to message them? What time are you going to meet? What is the address you want everyone to meet at? What gift are you going to bring? Are you going to go together in one car or meet there separately?",
+    "front_description":
+        "Often hosted near Christmas is a party game with a unique set of rules where amusing and impractical gifts are exchanged.",
+    "color": [
+      Colors.red.shade300,
+      Colors.red.shade900,
+    ]
+  },
+  {
+    "front_title": "Musical Chairs",
+    "back_title": "Details To Consider",
+    "back_description":
+        "Who do you want to come? What application will you use to message them? What time are you going to meet? What is the address you want everyone to meet at? Are you going to go together in one car or meet there separately?",
+    "front_description":
+        "Is a party game in which players compete for a decreasing number of chairs, those in successive rounds unable to find a chair to sit on are out of the game when the music is abruptly stopped.",
+    "color": [
+      Colors.orange.shade300,
+      Colors.orange.shade700,
+    ]
+  },
+  {
+    "front_title": "Ski Trip",
+    "back_title": "Details To Consider",
+    "back_description":
+        "Who do you want to come? What application will you use to message them? What time are you going to meet? What is the address you want everyone to meet at? What is name of the place we are going to Skiing? What time should we leave to go up? What time should we come back? How many days will we stay? Are you going to go together in one car or meet there separately?",
+    "front_description":
+        "This is a journey likely to the snow with the intention of going for a set duration of time. Often people will Snowboard or Ski during the trip.",
+    "color": [
+      Colors.teal.shade500,
+      Colors.teal.shade200,
+    ]
+  },
+  {
+    "front_title": "Bowling",
+    "back_title": "Details To Consider",
+    "back_description":
+        "Who do you want to come? What application will you use to message them? What time are you going to meet? What is the address you want everyone to meet at? What is name of the place we are going bowling? What time should we leave? What time should we come back? How many games do we want to play? Are you going to go together in one car or meet there separately?",
+    "front_description":
+        "Is a game in which you roll a heavy ball down a narrow lane to know down a group of pins. Bumpers can help assist the young folks when they are starting out.",
+    "color": [
+      Colors.purple.shade800,
+      Colors.purple.shade400,
+    ]
+  },
+  {
+    "front_title": "Golfing",
+    "back_title": "Details To Consider",
+    "back_description":
+        "Who do you want to come? What application will you use to message them? What time are you going to meet? What is the address you want everyone to meet at? Is there a dress code? What is the name of the place we are going golfing? What time should we plan be there? Are you going to go together in one car or meet there separately?",
+    "front_description":
+        "Is a game in which a player using special clubs attempts to sink a ball with as few strokes as possible usually on  9 or 18 holes on a course. People whom are newer to the game can try the driving range or an executive golf course where the holes are shorter.",
+    "color": [
+      Colors.greenAccent.shade100,
+      Colors.greenAccent.shade700,
+    ]
+  },
+  {
+    "front_title": "Board Game",
+    "back_title": "Details To Consider",
+    "back_description":
+        "Who do you want to come? What application will you use to message them? What time are you going to meet? What is the address you want everyone to meet at? What is the name of the place we are going? What time should we leave to get there? Are you going to go together in one car or meet there separately?",
+    "front_description":
+        "Is any game played on a board, especially one that involves the movement of pieces. There are many options to choose from.",
+    "color": [
+      Colors.blueGrey.shade100,
+      Colors.blueGrey.shade400,
+    ]
+  },
+  {
+    "front_title": "Fishing",
+    "back_title": "Details To Consider",
+    "back_description":
+        "Who do you want to come? What application will you use to message them? What time are you going to meet? What is the address you want everyone to meet at? What is the name of the place we are going fishing? Do I have the proper fishing gear needed? Where can I go to get the proper fishing gear? Are you going to go together in one car or meet there separately?",
+    "front_description":
+        "Is the activity of trying to catch fish. Typically includes fishing gear such as a fishing pole, hook, and bait.",
+    "color": [
+      Colors.cyan.shade800,
+      Colors.cyan.shade600,
+    ]
+  },
+  {
+    "front_title": "Volleyball",
+    "back_title": "Details To Consider",
+    "back_description":
+        "Who do you want to come? What application will you use to message them? What time are you going to meet? What is the address you want everyone to meet at? What is the name of the place we are going golfing? Do I have the proper fishing gear needed? Where can I go to get the proper fishing gear? Are you going to go together in one car or meet there separately?",
+    "front_description":
+        "Is a team sport in which two teams of six players are separated by a net. Each team tries to score points by grounding a ball on the other team's court under organized rules.",
+    "color": [
+      Colors.amberAccent.shade700,
+      Colors.amberAccent.shade400,
+    ]
+  },
+  {
+    "front_title": "Amusement Park",
+    "back_title": "Details To Consider",
+    "back_description":
+        "Who do you want to come? What application will you use to message them? What time are you going to meet? What is the address you want everyone to meet at? What is the name of the park? Are you going to go together in one car or meet there separately?",
+    "front_description":
+        "Is a park that features various attractions, such as rides and games, as well as other events for entertainment purposes.",
+    "color": [
+      Colors.yellow.shade900,
+      Colors.yellow.shade800,
+    ]
+  },
+  {
+    "front_title": "Rock Climbing",
+    "back_title": "Details To Consider",
+    "back_description":
+        "Who do you want to come? What application will you use to message them? What time are you going to meet? What is the address you want everyone to meet at? What is the name of the rock climbing place? Are you going to go together in one car or meet there separately?",
+    "front_description":
+        "Consists climbing rock faces, especially with the aid of ropes and special equipment. There is also indoor options where you can do bouldering on artificial structures.",
+    "color": [
+      Colors.brown.shade300,
+      Colors.brown.shade800,
+    ]
+  },
+  {
+    "front_title": "Sky Diving",
+    "back_title": "Details To Consider",
+    "back_description":
+        "Who do you want to come? What application will you use to message them? What time are you going to meet? What is the address you want everyone to meet at? What is the name of the sky diving place? Are you going to go together in one car or meet there separately?",
+    "front_description":
+        "Is the activity of jumping from an aircraft and performing acrobatic maneuvers in the air during free fall before landing by parachute.",
+    "color": [
+      Colors.lightBlue.shade700,
+      Colors.lightBlue.shade400,
+    ]
+  },
+  {
+    "front_title": "Biking",
+    "back_title": "Details To Consider",
+    "back_description":
+        "Who do you want to come? What application will you use to message them? What time are you going to meet? What is the address you want everyone to meet at? What is the name of the place you want to meet? Are you bringing your own bike? Will anyone need to rent or borrow one? Will you going to go together in one car or meet there separately? Does the car have enough room to fit the bikes?",
+    "front_description":
+        "To go somewhere by Bicycle. This can be nice for sight seeing while getting exercise. There are different types some might be more useful for the experience you intend to have it for.",
+    "color": [
+      Colors.grey.shade400,
+      Colors.grey.shade700,
+    ]
+  },
+  {
+    "front_title": "Beach Visit",
+    "back_title": "Details To Consider",
+    "back_description":
+        "Who do you want to come? What application will you use to message them? What time are you going to meet? What is the address you want everyone to meet at? What is the name of the beach? Are you going to go together in one car or meet there separately?",
+    "front_description":
+        "A pebbly or sandy shore, often by the ocean fluctuating between high and low water marks.",
+    "color": [
+      Colors.grey.shade400,
+      Colors.grey.shade700,
+    ]
+  },
+  {
+    "front_title": "Cooking",
+    "back_title": "Details To Consider",
+    "back_description":
+        "Who do you want to come? What application will you use to message them? What time are you going to meet? What is the address you want everyone to meet at? What ingredients will you need? Do you have the proper cooking equipment? Do you want people to bring anything? What should each person bring?",
+    "front_description":
+        "The practice or skill of preparing food by combining, mixing, and heating ingredients. Can be an opportunity to explore trying a new meal with fancy preparation.",
+    "color": [
+      Colors.blueGrey.shade400,
+      Colors.blueGrey.shade900,
+    ]
+  },
 ];
